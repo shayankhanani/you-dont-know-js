d91692308b25ebf912adf589ece819294e176e4c
Shayan Khanani <shayan.khanani@outlook.com>
2020-02-12 00:55:03 +0500

    Explicit coercion is simply that you can see from the code that a conversion from one type to another will occur, whereas implicit coercion is when the type conversion can happen as more of a non obvious side effect of some other operation.

---
+++ Up & Going/chapter-2/chp-2-topic-3-coercion.js
@@ -0,0 +1,14 @@
+//Explicit Coercion
+var a = "42";
+var b = Number(a);
+
+console.log(typeof a);
+console.log(typeof b);
+
+//Implicit Coercion
+
+a = "36";
+b = a * 1;
+
+console.log(typeof a);
+console.log(typeof b);
